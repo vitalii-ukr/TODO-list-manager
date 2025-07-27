21be6082fed2323b664697d3623903be3b2d98fd
Bizon <v.stovpovyi@gmail.com>
2025-07-27 13:15:48 -0500

    oportunity to add new todo item

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css';
-import './TodoList';
+import TodoForm from './TodoForm';
 import TodoList from './TodoList';
 
 function App() {
   return (
     <div>
       <h1>My Todos</h1>
+      <TodoForm />
       <TodoList />
     </div>
   );
