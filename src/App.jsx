d012bf5011ea06c2d6058a2296e4cd47d7689883
Bizon <v.stovpovyi@gmail.com>
2025-07-31 12:03:08 -0500

    state is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
+import { useState } from 'react';
 import './App.css';
 import TodoForm from './TodoForm';
 import TodoList from './TodoList';
 
 function App() {
+  const [newTodo, setNewTodo] = useState('initial value');
+
   return (
     <div>
       <h1>My Todos</h1>
       <TodoForm />
+      <p>{newTodo}</p>
       <TodoList />
     </div>
   );
