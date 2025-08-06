2cf377c28a396dc09feebb27f19aff3aeb0bef0a
Bizon <v.stovpovyi@gmail.com>
2025-08-06 11:41:20 -0500

    added new todos

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,17 @@ import TodoForm from './TodoForm';
 import TodoList from './TodoList';
 
 function App() {
-  const [newTodo, setNewTodo] = useState('initial value');
+  const [todoList, setTodoList] = useState([]);
+
+  function addTodo(title) {
+    const newTodo = { title: title, id: Date.now() };
+    setTodoList([...todoList, newTodo]);
+  }
 
   return (
     <div>
       <h1>My Todos</h1>
-      <TodoForm />
-      <p>{newTodo}</p>
+      <TodoForm onAddTodo={addTodo} />
       <TodoList />
     </div>
   );
