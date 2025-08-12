a2c9a2599173b459d46c4f74ea383d85700eaabc
Bizon <v.stovpovyi@gmail.com>
2025-08-12 12:17:49 -0500

    fix unvisible message for empty list

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -3,20 +3,22 @@ import TodoListItem from './TodoListItem';
 function TodoList({ todoList, onCompleteTodo }) {
   return (
     <ul>
-      {todoList.length > 0 ? (
-        (() => {
-          const filteredTodoList = todoList.filter((t) => !t.isCompleted);
-          return filteredTodoList.map((todo) => (
-            <TodoListItem
-              key={todo.id}
-              todo={todo}
-              onCompleteTodo={onCompleteTodo}
-            />
-          ));
-        })()
-      ) : (
-        <p>Add todo above to get started</p>
-      )}
+      {(() => {
+        const filteredTodoList = todoList.filter((t) => !t.isCompleted);
+        return filteredTodoList.length > 0 ? (
+          (() => {
+            return filteredTodoList.map((todo) => (
+              <TodoListItem
+                key={todo.id}
+                todo={todo}
+                onCompleteTodo={onCompleteTodo}
+              />
+            ));
+          })()
+        ) : (
+          <p>Add todo above to get started</p>
+        );
+      })()}
     </ul>
   );
 }
