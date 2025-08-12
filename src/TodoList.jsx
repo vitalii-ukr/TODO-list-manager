8a04bc238a3ed05c6c46d43a84c9f125c5ea9686
Bizon <v.stovpovyi@gmail.com>
2025-08-12 10:55:44 -0500

    updated by requirements

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -4,15 +4,16 @@ function TodoList({ todoList, onCompleteTodo }) {
   return (
     <ul>
       {todoList.length > 0 ? (
-        todoList
-          .filter((t) => !t.isCompleted)
-          .map((todo) => (
+        (() => {
+          const filteredTodoList = todoList.filter((t) => !t.isCompleted);
+          return filteredTodoList.map((todo) => (
             <TodoListItem
               key={todo.id}
               todo={todo}
               onCompleteTodo={onCompleteTodo}
             />
-          ))
+          ));
+        })()
       ) : (
         <p>Add todo above to get started</p>
       )}
