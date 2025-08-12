f55e64d387d94f7c3c53254d75f811591163c928
Bizon <v.stovpovyi@gmail.com>
2025-08-12 10:35:32 -0500

    option is added for complete todo

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,19 @@ import TodoList from './TodoList';
 function App() {
   const [todoList, setTodoList] = useState([]);
 
+  function completedTodo(id) {
+    const updatedTodos = todoList.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, isCompleted: true };
+      }
+      return todo;
+    });
+
+    setTodoList(updatedTodos);
+  }
+
   function addTodo(title) {
-    const newTodo = { title: title, id: Date.now() };
+    const newTodo = { title: title, id: Date.now(), isCompleted: false };
     setTodoList([...todoList, newTodo]);
   }
 
@@ -15,7 +26,7 @@ function App() {
     <div>
       <h1>My Todos</h1>
       <TodoForm onAddTodo={addTodo} />
-      <TodoList todoList={todoList} />
+      <TodoList todoList={todoList} onCompleteTodo={completedTodo} />
     </div>
   );
 }
