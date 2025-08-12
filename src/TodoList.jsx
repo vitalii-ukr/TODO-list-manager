1c44adbd9ab46c92ecab5c5d0904e9a65e63f428
Bizon <v.stovpovyi@gmail.com>
2025-08-12 12:19:57 -0500

    refactoring

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,24 +1,23 @@
 import TodoListItem from './TodoListItem';
 
 function TodoList({ todoList, onCompleteTodo }) {
+  const filteredTodoList = todoList.filter((t) => !t.isCompleted);
+
   return (
     <ul>
-      {(() => {
-        const filteredTodoList = todoList.filter((t) => !t.isCompleted);
-        return filteredTodoList.length > 0 ? (
-          (() => {
-            return filteredTodoList.map((todo) => (
-              <TodoListItem
-                key={todo.id}
-                todo={todo}
-                onCompleteTodo={onCompleteTodo}
-              />
-            ));
-          })()
-        ) : (
-          <p>Add todo above to get started</p>
-        );
-      })()}
+      {filteredTodoList.length > 0 ? (
+        (() => {
+          return filteredTodoList.map((todo) => (
+            <TodoListItem
+              key={todo.id}
+              todo={todo}
+              onCompleteTodo={onCompleteTodo}
+            />
+          ));
+        })()
+      ) : (
+        <p>Add todo above to get started</p>
+      )}
     </ul>
   );
 }
